0067796e59ac28a80c477d3a5be4796b4d92168b
Eoin Farren <eoinfarren@gmail.com>
2022-11-16 20:51:40 +0100

    Yelp MVP unfinished

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+  # get 'reviews/new'
+  # get 'restaurant/new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  # get 'restaurants', to: 'restaurants'
+
+  resources :restaurants, only: [:index, :new, :create, :show] do
+    resources :reviews, only: [:new, :create]
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+dishoom = {name: "Dishoom", address: "7 Boundary St, London E2 7JE", phone_number: "323232323", category: "italian"}
+pizza_east =  {name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", phone_number: "234324343", category: "french"}
+carluccios =  {name: "Carluccios", address: "32 Main St, London SW1 6CE", phone_number:"4343143", category: "french"}
+nandos =  {name: "Nandos", address: "12 Great Portland St, London E4 2OP", phone_number:"4343143", category: "belgian"}
+honest =  {name: "Honest", address: "56 Acre Lane, London SE1 5TZ", phone_number:"87978787", category: "japanese"}
+
+[dishoom, pizza_east, carluccios, nandos, honest].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
